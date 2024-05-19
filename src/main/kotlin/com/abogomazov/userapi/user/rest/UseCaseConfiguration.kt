7fdbed936ebf80f3ca551469f2e56a085abfe274
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2024-05-19 10:45:26 +0200

    add CreateNewUser usecase configuration

--- src/main/kotlin/com/abogomazov/userapi/user/rest/UseCaseConfiguration.kt
+++ src/main/kotlin/com/abogomazov/userapi/user/rest/UseCaseConfiguration.kt
@@ -1,7 +1,11 @@
 package com.abogomazov.userapi.user.rest
 
+import com.abogomazov.userapi.user.domain.UserAlreadyExist
+import com.abogomazov.userapi.user.usecase.CreateNewUser
 import com.abogomazov.userapi.user.usecase.GetAllUsers
 import com.abogomazov.userapi.user.usecase.UserExtractor
+import com.abogomazov.userapi.user.usecase.UserPersister
+import com.abogomazov.userapi.user.usecase.invariant.UserAlreadyExistsInvariant
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -10,4 +14,15 @@ class UseCaseConfiguration {
     @Bean
     fun getAllUsers(extractor: UserExtractor) =
         GetAllUsers(extractor)
+
+    @Bean
+    fun createNewUser(
+        userAlreadyExists: UserAlreadyExist,
+        persister: UserPersister,
+    ) = CreateNewUser(userAlreadyExists, persister)
+
+    @Bean
+    fun userAlreadyExists(
+        extractor: UserExtractor,
+    ) = UserAlreadyExistsInvariant(extractor)
 }
