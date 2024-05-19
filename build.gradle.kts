31d6b97671a9e0c9015dcf85338d4ddfa089850a
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2024-05-19 10:46:41 +0200

    tidy up dependency config

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,25 +12,30 @@ repositories {
 
 // TODO autoupdate deps
 dependencies {
+    // COMMON
     implementation("org.springframework.boot:spring-boot-starter:3.2.5")
-    implementation("org.springframework.boot:spring-boot-starter-web:3.2.5")
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc:3.2.5")
     implementation("io.arrow-kt:arrow-core:1.2.4")
-    runtimeOnly("org.postgresql:postgresql:42.7.3")
-    runtimeOnly("org.flywaydb:flyway-database-postgresql:10.13.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.5")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers:3.2.5")
     testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.4.0")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:5.9.0")
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.9.0")
-    testImplementation("org.testcontainers:postgresql:1.19.8")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testImplementation("com.tngtech.archunit:archunit:1.3.0")
-    testImplementation("com.ninja-squad:springmockk:4.0.2")
     testImplementation("io.mockk:mockk:1.13.11")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
 
+    // REST
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.5")
+    implementation("org.springframework.hateoas:spring-hateoas:2.3.0")
 
+    // PERSISTENCE
+    runtimeOnly("org.postgresql:postgresql:42.7.3")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql:10.13.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc:3.2.5")
+
+    testImplementation("org.springframework.boot:spring-boot-testcontainers:3.2.5")
+    testImplementation("org.testcontainers:postgresql:1.19.8")
 }
 
 tasks.test {
