f75300597ed56a2fb9d7ef6982f1b73caaf5125c
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2024-05-19 10:46:07 +0200

    add api routes

--- src/main/kotlin/com/abogomazov/userapi/user/rest/GetAllUsersEndpoint.kt
+++ src/main/kotlin/com/abogomazov/userapi/user/rest/GetAllUsersEndpoint.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("api/v1/users")
+@RequestMapping(USERS_RESOURCE)
 class GetAllUsersEndpoint(
     private val usecase: GetAllUsers,
 ) {
---
+++ src/main/kotlin/com/abogomazov/userapi/user/rest/Routes.kt
@@ -0,0 +1,7 @@
+package com.abogomazov.userapi.user.rest
+
+const val API_PREFIX = "api"
+const val API_VERSION = "v1"
+const val RESOURCE_PREFIX = "/$API_PREFIX/$API_VERSION"
+
+const val USERS_RESOURCE = "$RESOURCE_PREFIX/users"
