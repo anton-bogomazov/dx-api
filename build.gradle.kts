16bf7297af08e4373c8ed3b95a663fa4c5b3ee33
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2024-05-20 22:58:23 +0200

    optimize configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("jvm") version "1.9.23"
     id("org.jetbrains.kotlin.plugin.spring") version "1.9.24"
     id("com.google.cloud.tools.jib") version "3.4.2"
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
 
 group = "com.abogomazov"
@@ -23,32 +24,35 @@ jib {
     }
 }
 
+val springVer = "3.2.5"
+val kotestVer = "5.9.0"
+
 // TODO autoupdate deps
 dependencies {
     // COMMON
-    implementation("org.springframework.boot:spring-boot-starter:3.2.5")
+    implementation("org.springframework.boot:spring-boot-starter:$springVer")
     implementation("io.arrow-kt:arrow-core:1.2.4")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.5")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:$springVer")
     testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.4.0")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.9.0")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.9.0")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVer")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVer")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testImplementation("com.tngtech.archunit:archunit:1.3.0")
     testImplementation("io.mockk:mockk:1.13.11")
     testImplementation("com.ninja-squad:springmockk:4.0.2")
 
     // REST
-    implementation("org.springframework.boot:spring-boot-starter-web:3.2.5")
+    implementation("org.springframework.boot:spring-boot-starter-web:$springVer")
     implementation("org.springframework.hateoas:spring-hateoas:2.3.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
 
     // PERSISTENCE
     runtimeOnly("org.postgresql:postgresql:42.7.3")
     runtimeOnly("org.flywaydb:flyway-database-postgresql:10.13.0")
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc:3.2.5")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc:$springVer")
 
-    testImplementation("org.springframework.boot:spring-boot-testcontainers:3.2.5")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers:$springVer")
     testImplementation("org.testcontainers:postgresql:1.19.8")
 }
 
