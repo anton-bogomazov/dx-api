c2fb69409a66ea249ea7867905346ac4a92933bc
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2024-05-20 23:40:11 +0200

    add dependency update plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import io.gitlab.arturbosch.detekt.Detekt
 
 plugins {
@@ -6,6 +7,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.spring") version "1.9.24"
     id("com.google.cloud.tools.jib") version "3.4.2"
     id("io.gitlab.arturbosch.detekt") version "1.23.6"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 group = "com.abogomazov"
@@ -58,15 +60,28 @@ dependencies {
     testImplementation("org.testcontainers:postgresql:1.19.8")
 }
 
+// DETEKT CONFIG
 detekt {
     buildUponDefaultConfig = true
     allRules = false
     config.setFrom("$projectDir/tools/detekt/detekt.yml")
 }
-
 tasks.withType<Detekt>().configureEach {
     reports {
         html.required.set(true)
+    }
+}
+
+// DEPENDENCY UPDATE CONFIG
+tasks.withType<DependencyUpdatesTask> {
+    fun isNonStable(version: String): Boolean {
+        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+        val isStable = stableKeyword || regex.matches(version)
+        return isStable.not()
+    }
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
     }
 }
 
