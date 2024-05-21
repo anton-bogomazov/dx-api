d80ac99e203e8d67e00f44992c00aa307de4e626
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2024-05-21 19:37:00 +0200

    move project to submodule

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,99 +1,58 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import io.gitlab.arturbosch.detekt.Detekt
 
+val parentProjectDir = projectDir
+
 plugins {
-    application
-    kotlin("jvm") version "1.9.24"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.9.24"
-    id("com.google.cloud.tools.jib") version "3.4.2"
+    kotlin("jvm") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.plugin.spring") version "1.9.24" apply false
+    id("com.google.cloud.tools.jib") version "3.4.2" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.6"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
-    id("com.github.ben-manes.versions") version "0.51.0"
-    id("java-test-fixtures")
+    id("com.github.ben-manes.versions") version "0.51.0" apply false
 }
 
-group = "com.abogomazov"
-version = "1.0-SNAPSHOT"
-val mainClass by extra("com.abogomazov.userapi.MainKt")
+subprojects {
 
-repositories {
-    mavenCentral()
-}
+    group = "com.abogomazov"
+    version = "1.0-SNAPSHOT"
 
-jib {
-    container {
-        ports = listOf("8080")
-        mainClass = mainClass
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+        plugin("com.google.cloud.tools.jib")
+        plugin("io.gitlab.arturbosch.detekt")
+        plugin("org.jlleitschuh.gradle.ktlint")
+        plugin("com.github.ben-manes.versions")
+        plugin("java-test-fixtures")
     }
-    to {
-        image = "abogomazov/user-api"
+
+    repositories {
+        mavenCentral()
     }
-}
-
-val springVer = "3.2.5"
-val kotestVer = "5.9.0"
-
-// TODO autoupdate deps
-dependencies {
-    // COMMON
-    implementation("org.springframework.boot:spring-boot-starter:$springVer")
-    implementation("io.arrow-kt:arrow-core:1.2.4")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test:$springVer")
-    testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.4.0")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVer")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVer")
-    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
-    testImplementation("com.tngtech.archunit:archunit:1.3.0")
-    testImplementation("io.mockk:mockk:1.13.11")
-    testImplementation("com.ninja-squad:springmockk:4.0.2")
-
-    testImplementation(testFixtures(rootProject))
-    testFixturesImplementation("io.kotest.extensions:kotest-assertions-arrow:1.4.0")
-    testFixturesImplementation("org.springframework.boot:spring-boot-starter-test:$springVer")
-
-    // REST
-    implementation("org.springframework.boot:spring-boot-starter-web:$springVer")
-    implementation("org.springframework.hateoas:spring-hateoas:2.3.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
-
-    // PERSISTENCE
-    runtimeOnly("org.postgresql:postgresql:42.7.3")
-    runtimeOnly("org.flywaydb:flyway-database-postgresql:10.13.0")
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc:$springVer")
-
-    testImplementation("org.springframework.boot:spring-boot-testcontainers:$springVer")
-    testImplementation("org.testcontainers:postgresql:1.19.8")
-}
 
 // DETEKT CONFIG
-detekt {
-    buildUponDefaultConfig = true
-    allRules = false
-    config.setFrom("$projectDir/tools/detekt/detekt.yml")
-}
-tasks.withType<Detekt>().configureEach {
-    reports {
-        html.required.set(true)
+    detekt {
+        buildUponDefaultConfig = true
+        allRules = false
+        config.setFrom("$parentProjectDir/tools/detekt/detekt.yml")
     }
-}
+    tasks.withType<Detekt>().configureEach {
+        reports {
+            html.required.set(true)
+        }
+    }
 
 // DEPENDENCY UPDATE CONFIG
-tasks.withType<DependencyUpdatesTask> {
-    fun isNonStable(version: String): Boolean {
-        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
-        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-        val isStable = stableKeyword || regex.matches(version)
-        return isStable.not()
+    tasks.withType<DependencyUpdatesTask> {
+        fun isNonStable(version: String): Boolean {
+            val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+            val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+            val isStable = stableKeyword || regex.matches(version)
+            return isStable.not()
+        }
+        rejectVersionIf {
+            isNonStable(candidate.version) && !isNonStable(currentVersion)
+        }
     }
-    rejectVersionIf {
-        isNonStable(candidate.version) && !isNonStable(currentVersion)
-    }
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
 }
