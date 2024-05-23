a515cd806b9f3dd9dff4d1f75bd95c488df41f72
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2024-05-23 10:42:23 +0200

    delete docker build from build stage

--- tools/scripts/build.sh
+++ tools/scripts/build.sh
@@ -3,5 +3,4 @@ set -e
 currentDir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 rootDir="$currentDir/../../"
 
-# TODO add it to build phase
-(cd "$rootDir" && exec ./gradlew build user-api:jibDockerBuild -PallWarningsAsErrors=true)
+(cd "$rootDir" && exec ./gradlew build -PallWarningsAsErrors=true)
