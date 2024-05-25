04f636e5cb516eecbf5655c50bd4a2507874aafe
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2024-05-25 16:56:38 +0200

    add fitnesse package

---
+++ user-api/application/src/test/kotlin/com/abogomazov/userapi/user/fitnesse/ArchTest.kt
@@ -0,0 +1,30 @@
+package com.abogomazov.userapi.user.fitnesse
+
+import com.tngtech.archunit.core.importer.ClassFileImporter
+import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests
+import com.tngtech.archunit.library.Architectures.onionArchitecture
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices
+import io.kotest.core.spec.style.StringSpec
+
+class ArchTest : StringSpec({
+
+    val classes =
+        ClassFileImporter()
+            .withImportOption(DoNotIncludeTests())
+            .importPackages("com.abogomazov.userapi.user")
+
+    "application follows onion arch rules" {
+        onionArchitecture()
+            .domainModels("com.abogomazov.userapi.user.domain..")
+            .domainServices("com.abogomazov.userapi.user.domain..")
+            .applicationServices("com.abogomazov.userapi.user.usecase..")
+            .adapter("Postgres Persistence", "..persistence.postgres..")
+            .adapter("Rest API", "..rest..")
+            .check(classes)
+    }
+
+    "free of cycle dependencies" {
+        slices().matching("com.abogomazov.userapi.(**)")
+            .should().beFreeOfCycles()
+    }
+})
