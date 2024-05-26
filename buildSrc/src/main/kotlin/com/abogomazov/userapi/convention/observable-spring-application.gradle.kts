758381a1c51168489e7f0d09ada606fa34c81825
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2024-05-26 19:39:16 +0200

    add log monitoring

--- buildSrc/src/main/kotlin/com/abogomazov/userapi/convention/observable-spring-application.gradle.kts
+++ buildSrc/src/main/kotlin/com/abogomazov/userapi/convention/observable-spring-application.gradle.kts
@@ -9,4 +9,5 @@ dependencies {
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("de.codecentric:spring-boot-admin-starter-client:3.2.3")
     implementation("org.springframework.cloud:spring-cloud-starter-sleuth:3.1.11")
+    implementation("com.github.loki4j:loki-logback-appender:1.5.1")
 }
